4ae12f81af0c4885c483d4705c7b30d5355cab76
Immortan <2633866263@qq.com>
2021-08-12 13:35:30 +0800

    First commit

---
+++ Random Int Generator/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args){
+        Random r = new Random(0);
+        System.out.println(r.nextInt(2));
+    }
+}
---
+++ Test/src/Extended.java
@@ -0,0 +1,3 @@
+public class Extended extends Base{
+
+}
