57d03f8507d5425178df06894b3ac9369da4fc91
Immortan <2633866263@qq.com>
2021-08-16 14:15:43 +0800

    ""

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main(String []args){
-        Extended p = new Extended();
-        System.out.println(p.Outx());
+    public static void main(String[] args){
+
     }
 }
