119a40c788a118ad77c3d8fd9f4836b3917833eb
Immortan <2633866263@qq.com>
2021-10-21 20:00:55 +0800

    first commit

--- Sort Method/Heap Sort/src/Heap.java
+++ Sort Method/Heap Sort/src/Heap.java
@@ -3,51 +3,71 @@
  * */
 
 public class Heap {
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public static void heapsort_asc(int[] nums) {
+        buildMaxHeap(nums);
+
+        for (int i=nums.length - 1; i>0; i--) {
+            swap(nums, i, 0);
+            maxHeapify(nums, 0, i);
+        }
+    }
+
+    public static void  heapsort_des(int[] nums) {
+        buildMinHeap(nums);
+
+        for (int i=nums.length - 1; i>0; i--) {
+            swap(nums, i, 0);
+            minHeapify(nums, 0, i);
+        }
+    }
+
     public static void buildMaxHeap(int[] nums) {
         for (int i = nums.length / 2 - 1; i>=0; i--)
-            maxHeapify(nums, i);
+            maxHeapify(nums, i, nums.length);
     }
 
     public static void buildMinHeap(int[] nums) {
         for (int i = nums.length / 2 - 1; i>=0; i--)
-            minHeapify(nums, i);
+            minHeapify(nums, i, nums.length);
     }
 
-    public static void maxHeapify(int[] heap, int index) {
+    public static void maxHeapify(int[] heap, int index, int length) {
         int left = index * 2 + 1;
         int right = index * 2 + 2;
 
         int largest = index;
 
-        if (left < heap.length)
+        if (left < length)
             largest = heap[left] > heap[largest] ? left : largest;
-        if (right < heap.length)
+        if (right < length)
             largest = heap[right] > heap[largest] ? right : largest;
 
         if (largest != index) {
-            int temp = heap[largest];
-            heap[largest] = heap[index];
-            heap[index] = temp;
-            maxHeapify(heap, largest);
+            swap(heap, index, largest);
+            maxHeapify(heap, largest, length);
         }
     }
 
-    public static void minHeapify(int[] heap, int index) {
+    public static void minHeapify(int[] heap, int index, int length) {
         int left = index * 2 + 1;
         int right = index * 2 + 2;
 
         int smallest = index;
 
-        if (left < heap.length)
+        if (left < length)
             smallest = heap[left] < heap[smallest] ? left : smallest;
-        if (right < heap.length)
+        if (right < length)
             smallest = heap[right] < heap[smallest] ? right : smallest;
 
         if (smallest != index) {
-            int temp = heap[smallest];
-            heap[smallest] = heap[index];
-            heap[index] = temp;
-            minHeapify(heap, smallest);
+            swap(heap, index, smallest);
+            minHeapify(heap, smallest, length);
         }
     }
 }
