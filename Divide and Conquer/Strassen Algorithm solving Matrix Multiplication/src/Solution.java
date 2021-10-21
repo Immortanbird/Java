940328200121397f98bd50cc0d536e643d49e18b
Immortan <2633866263@qq.com>
2021-10-21 18:55:36 +0800

    first commit

---
+++ Divide and Conquer/Strassen Algorithm solving Matrix Multiplication/src/Solution.java
@@ -0,0 +1,9 @@
+/*
+* Calculate the product of two square matrices both having n rows and n columns.
+* */
+
+public class Solution {
+    public static void SquareMatrixMultiply(int[][] A, int[][] B, int[][] C) {
+
+    }
+}
