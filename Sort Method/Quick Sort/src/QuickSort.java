9fcdae45c059688dc2985e5f01c92dac71777dec
Immortan <2633866263@qq.com>
2021-10-25 18:57:51 +0800

    first commit

---
+++ Sort Method/Quick Sort/src/QuickSort.java
@@ -0,0 +1,35 @@
+/*
+* Quicksort, ascending.
+* */
+
+public class QuickSort {
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static int partition(int[] nums, int start, int end) {
+        int i = start + 1;
+        int j = end;
+
+        while (i <= j){
+            if (nums[i] > nums[start])
+                swap(nums, i, j--);
+            else
+                i++;
+        }
+
+        swap(nums, start, i - 1);
+
+        return i - 1;
+    }
+
+    public static void QuickSort(int[] nums, int start, int end) {
+        if (start < end) {
+            int pivot = partition(nums, start, end);
+            QuickSort(nums, start, pivot - 1);
+            QuickSort(nums, pivot + 1, end);
+        }
+    }
+}
