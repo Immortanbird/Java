ecc2cd466a3151831e7a9aa35e02552ba92f557f
Immortan <2633866263@qq.com>
2021-10-28 21:42:00 +0800

    first commit

---
+++ Sort Method/Counting Sort/src/CountingSort.java
@@ -0,0 +1,25 @@
+/*
+* Counting sort
+* Assuming that all integers in array "nums" are smaller than or equals to k, we may sort the array by counting the number of numbers smaller than i (0<=i<=k) to determine the position of i.
+* The method needs additional space of k+nums.length.
+* */
+
+public class CountingSort {
+    public static int[] countingSort(int[] nums, int k) {
+        int[] res = new int[nums.length];
+        int[] count = new int[k + 1];
+
+        for (int x : nums)
+            count[x]++;
+
+        for (int i=1; i<=k; i++)
+            count[i] += count[i-1];
+
+        for (int x : nums) {
+            res[count[x] - 1] = x;
+            count[x] -= 1;  //in case that the repeated numbers won't be set at the same position
+        }
+
+        return res;
+    }
+}
